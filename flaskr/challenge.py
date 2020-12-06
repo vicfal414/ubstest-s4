c6c714afa5c85e8dbce7432ad6a03afd401c3ad0
Urjitha <urjithamuthiah@gmail.com>
2020-12-06 16:44:53 -0500

    All of the new code with most updated main.py?

---
+++ flaskr/challenge.py
@@ -0,0 +1,7 @@
+newChallenge = {
+    'name': "", 
+    'description': "challengeTasks", 
+    'duration': "challengeTasks",
+    'category': "challengeTasks",
+    'impact': "challengeTasks",
+    'suggestions': "challengeTasks"}
