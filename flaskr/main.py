db15df1550970225cd6074187092e86c73f25289
vicfal414 <36310124+vicfal414@users.noreply.github.com>
2020-12-06 14:06:23 -0500

    Update main.py

--- flaskr/main.py
+++ flaskr/main.py
@@ -32,7 +32,7 @@ def create_app(test_config=None):
     )
 
     # mysql.init_app(app)
-
+   
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -56,14 +56,18 @@ def create_app(test_config=None):
     def home():
         return render_template("index.html")
 
+    @app.route("/dash")
+    def dash():
+        return render_template("userdashboard.html")
+
     @app.route("/challenge")
     def chall():
         return render_template("challenge_list.html")
-
+    
     @app.route("/challenge1")
     def chall_pg1():
         return render_template("challenge_pages/challenge1.html")
-
+    
     @app.route("/challenge2")
     def chall_pg2():
         return render_template("challenge_pages/challenge2.html")
@@ -79,7 +83,7 @@ def create_app(test_config=None):
     @app.route("/challenge5")
     def chall_pg5():
         return render_template("challenge_pages/challenge5.html")
-
+    
     @app.route("/challenge6")
     def chall_pg6():
         return render_template("challenge_pages/challenge6.html")
@@ -89,8 +93,13 @@ def create_app(test_config=None):
     def chall_pg7():
         return render_template("challenge_pages/custom_challenge.html")
 
+    #Addition for new Custom Challenge specific page
+    @app.route("/challengeCustom")
+    def chall_pg8():
+        return render_template("new_custom_challenge.php")
+
     @app.route("/friends")
-    def friend():
+    def friend():  
         return render_template("friends.html",
             friendList=Users['friends'],
             notFriendList=Users['notFriends'])
@@ -112,9 +121,9 @@ def create_app(test_config=None):
             password = request.form['password']
             # cur = mysql.connection.cursor()
             connection = pymysql.connect(host='us-cdbr-east-02.cleardb.com',
-                             user='b2cb10b2b21b72',
-                             password='1b8b9cc5',
-                             db='heroku_318469e412eb0ae',
+                             user='b33b6415873ff5',
+                             password='d1a1b9a1',
+                             db='heroku_1e2700f5b989c0b',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
             with connection.cursor() as cursor:
@@ -125,16 +134,14 @@ def create_app(test_config=None):
                 session['logged_in'] = True
                 session['id'] = data['id']
                 session['username'] = data['username']
-                session['fname'] = data['fname']
-                session['lname'] = data['lname']
                 flash('You are logged in')
                 return redirect(url_for('home'))
             else:
                 msg = 'Invalid Credentials. Please try again.'
             connection.close()
         return render_template("login.html", msg = msg)
-
-
+    
+     
     @app.route('/logout')
     def logout():
         session.pop('logged_in', None)
@@ -152,12 +159,12 @@ def create_app(test_config=None):
             email = request.form['email']
             fname = request.form['fname']
             lname = request.form['lname']
-            connection = pymysql.connect(host='us-cdbr-east-02.cleardb.com',
-                             user='b2cb10b2b21b72',
-                             password='1b8b9cc5',
-                             db='heroku_318469e412eb0ae',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+            connection2 = pymysql.connect(host='us-cdbr-east-02.cleardb.com',
+                    user='b33b6415873ff5',
+                    password='d1a1b9a1',
+                    db='heroku_1e2700f5b989c0b',
+                    charset='utf8mb4',
+                    cursorclass=pymysql.cursors.DictCursor)
             with connection2.cursor() as cursor2:
                 cursor2.execute('SELECT * FROM accounts WHERE username = %s', (username,))
             data = cursor2.fetchone()
@@ -175,27 +182,60 @@ def create_app(test_config=None):
                 msg = 'Please enter your information.'
             else:
                 with connection2.cursor() as cursor3:
-                    cursor3.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s, NULL)', (fname, lname, username, password, email,))
-                    cursor3.execute('INSERT INTO dashboard VALUES (%s, NULL, NULL, NULL)', (username))
+                    cursor3.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s)', (fname, lname, username, password, email,))
                 connection2.commit()
                 msg = 'You have successfully registered!'
             connection2.close()
         elif request.method == 'POST':
             #Form is empty
             msg = 'Please enter your information.'
-
+        
         return render_template("signup.html", msg = msg)
+
+    #function for custom challenge form
+    @app.route("/addingcustom", methods = ['GET', 'POST'])
+    def addingcustom():
+        msg = ''
+        if request.method == 'POST' and 'challengeName' in request.form and 'shortDescription' in request.form and 'SelectDuration' in request.form and 'SelectCategory' in request.form and 'theImpact' in request.form and 'sugegstionsHelp' in request.form:
+            challengeName = request.form['challengeName']
+            shortDescription = request.form['shortDescription']
+            SelectDuration = request.form['SelectDuration']
+            SelectCategory = request.form['SelectCategory']
+            theImpact = request.form['theImpact']
+            sugegstionsHelp = request.form['sugegstionsHelp']
+            connection2 = pymysql.connect(host='us-cdbr-east-02.cleardb.com',
+                    user='b33b6415873ff5',
+                    password='d1a1b9a1',
+                    db='heroku_1e2700f5b989c0b',
+                    charset='utf8mb4',
+                    cursorclass=pymysql.cursors.DictCursor)
+            with connection2.cursor() as cursor3:
+                cursor3.execute("UPDATE challenges SET 
+                        name = challengeName,
+                        description = shortDescription,
+                        duration = SelectDuration,
+                        category = SelectCategory,
+                        impact =  theImpact,
+                        suggestions = sugegstionsHelp WHERE
+                        id = 1")
+            connection2.commit()
+            connection2.close()
+        elif request.method == 'POST':
+            #Form is empty
+            msg = 'Please enter all informaton for Custom Challenge.'
+        
+        return render_template("challenge_pages/custom_challenge.html", msg = msg)
 
     @app.route("/css")
     def css():
-        return render_template("static/css/style.css")
-
+        return render_template("static/css/style.css") 
+    
     @app.route("/random")
     def rand_chall():
         number = random.randint(1, 6)
         site = "chall_pg" + str(number)
         return redirect(url_for(site))
-
+        
     def getUserName(name):
         for uType in Users:
             for user in Users[uType]:
@@ -209,28 +249,7 @@ def create_app(test_config=None):
                 if user['name'] == name:
                     return user['profilePic']
         return None
-
-        @app.route("/dash")
-        def dash():
-            if session['logged_in'] == True:
-                connection = pymysql.connect(host='us-cdbr-east-02.cleardb.com',
-                    user='b2cb10b2b21b72',
-                    password='1b8b9cc5',
-                    db='heroku_318469e412eb0ae',
-                    charset='utf8mb4',
-                cursorclass=pymysql.cursors.DictCursor)
-                with connection.cursor() as cursor:
-                    cursor.execute('SELECT saved, progress, completed FROM dashboard WHERE user = %s', (session['username']))
-                challs = cursor.fetchone()
-                if challs:
-                    sav = challs['saved'].split("|")
-                    pro = challs['progress'].split("|")
-                    com = challs['completed'].split("|")
-                    session['saved'] = sav
-                    session['progress'] = pro
-                    session['completed'] = com
-            return render_template("userdashboard.html")
-
+    
     @app.route("/addFriend", methods = ['POST'])
     def addFriend():
         if request.method == 'POST':
@@ -239,7 +258,7 @@ def create_app(test_config=None):
             Users['friends'].append(newFriend)
             Users['notFriends'].remove(newFriend)
         return redirect(url_for('friend'))
-
+      
     @app.route("/remFriend", methods = ['POST'])
     def remFriend():
         if request.method == 'POST':
